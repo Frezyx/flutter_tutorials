0c3b142a528e64633b58bcc6d02d2c70e109bff7
Stanislav Ilin <youngfrezyx@gmail.com>
2023-01-13 10:22:16 +0500

    Initital commit

---
+++ android/app/src/main/kotlin/com/cryptoguys/crypto_coins_list/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.cryptoguys.crypto_coins_list
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,98 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+
+void main() {
+  runApp(const CryptoCurrenciesListApp());
+}
+
+class CryptoCurrenciesListApp extends StatelessWidget {
+  const CryptoCurrenciesListApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'CryptoCurrenciesList',
+      theme: ThemeData(
+        scaffoldBackgroundColor: const Color.fromARGB(255, 31, 31, 31),
+        primarySwatch: Colors.yellow,
+        dividerColor: Colors.white24,
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Color.fromARGB(255, 31, 31, 31),
+          titleTextStyle: TextStyle(
+            color: Colors.white,
+            fontSize: 20,
+            fontWeight: FontWeight.w700,
+          ),
+        ),
+        listTileTheme: const ListTileThemeData(iconColor: Colors.white),
+        textTheme: TextTheme(
+          bodyMedium: const TextStyle(
+            color: Colors.white,
+            fontWeight: FontWeight.w500,
+            fontSize: 20,
+          ),
+          labelSmall: TextStyle(
+            color: Colors.white.withOpacity(0.6),
+            fontWeight: FontWeight.w700,
+            fontSize: 14,
+          ),
+        ),
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({
+    super.key,
+  });
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('CryptoCurrenciesList'),
+      ),
+      body: ListView.separated(
+        itemCount: 10,
+        separatorBuilder: (context, index) => const Divider(),
+        itemBuilder: (context, i) => ListTile(
+          leading: SvgPicture.asset(
+            'assets/svg/bitcoin_logo.svg',
+            height: 30,
+            width: 30,
+          ),
+          title: Text(
+            'Bitcoin',
+            style: theme.textTheme.bodyMedium,
+          ),
+          subtitle: Text(
+            '200000\$',
+            style: theme.textTheme.labelSmall,
+          ),
+          trailing: const Icon(Icons.arrow_forward_ios),
+        ),
+      ),
+      // floatingActionButton: FloatingActionButton(
+      //   onPressed: _incrementCounter,
+      //   tooltip: 'Increment',
+      //   child: const Icon(Icons.add),
+      // ),
+    );
+  }
+}
